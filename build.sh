55b7d919bcd1aca68adb3dba7cc6cadcb8624804
Pablosandova <sandovalsotopablo@gmail.com>
2025-12-09 21:39:31 -0300

    Fix build script for Render Java environment

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install Java 17 if not present
+if [ ! -d "/opt/render/project/.jdks/openjdk17" ]; then
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+  sdk install java 17.0.8-tem
+fi
+
+# Build the project
 ./mvnw clean install -DskipTests
